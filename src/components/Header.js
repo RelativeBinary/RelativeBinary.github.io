f57121f362aadb44785dc1c5517ca007aaaeddf7
Pat B <patrick.bornay@gmail.com>
2022-01-10 23:17:34 +1100

    minor changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
-import { Button } from '@mui/material'
-import React from 'react'
+import { Button } from '@mui/material';
+import React from 'react';
 
 export default function Header() {
   return (
     <div>
-      Hi, I'm <Button> Patrick Bornay. </Button>
-
+      <div>
+        Hi, I'm <Button> Patrick Bornay. </Button>
+      </div>
       <h2>A web dev & cyber security enthusiast</h2>
     </div>
-  )
+  );
 }
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../components/Header';
 import Projects from '../components/Projects';
 import Footer from '../components/Footer';
+import Box from '@mui/material/Box';
+import Item from '@mui/material/Box';
 
 export default function Work() {
   return (
