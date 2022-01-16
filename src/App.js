31ea192c1ba8d4cba8ae78cc5264a24f742a5409
Pat B <patrick.bornay@gmail.com>
2022-01-16 23:11:10 +1100

    add some basic styles

--- src/App.js
+++ src/App.js
@@ -6,17 +6,26 @@ import Navbar from './components/Navbar';
 
 import { Routes, Route } from 'react-router-dom';
 import Testing from './components/Testing';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './styles/theme';
+import { GlobalStyles } from './styles/global';
+import { useContext } from 'react';
+import { StylesContext } from './contexts/StylesContext';
 
 function App() {
+  const themeContext = useContext(StylesContext);
   return (
-    <div className='App'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Work />}></Route>
-        <Route path='/about' element={<About />}></Route>
-        <Route path='/testing' element={<Testing />}></Route>
-      </Routes>
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <>
+        <GlobalStyles />
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Work />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/testing' element={<Testing />}></Route>
+        </Routes>
+      </>
+    </ThemeProvider>
   );
 }
 
