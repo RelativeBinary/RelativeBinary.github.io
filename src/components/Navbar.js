4573e2597af5207a6cca3cd1e7ec4561cde8caa1
Pat B <patrick.bornay@gmail.com>
2022-02-27 20:07:51 +1100

    PPR-4 rip out nav bar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,15 @@
 import React from 'react';
-import { AppBar, Toolbar, CssBaseline, makeStyles } from '@material-ui/core';
+import { AppBar, Toolbar, CssBaseline } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
-const useStyles = makeStyles((theme) => ({
-  navlinks: {
-    marginLeft: theme.spacing(10),
-    display: 'flex',
-  },
-  logo: {
-    flexGrow: '1',
-    cursor: 'pointer',
-  },
-  link: {
-    textDecoration: 'none',
-    color: 'white',
-    fontSize: '20px',
-    marginLeft: theme.spacing(20),
-    '&:hover': {
-      color: 'yellow',
-      borderBottom: '1px solid white',
-    },
-  },
-}));
-
 export default function Navbar() {
-  const classes = useStyles();
-
   return (
     <AppBar position='static'>
       <CssBaseline />
       <Toolbar>
-        <div className={classes.navlinks}>
-          <Link to='/' className={classes.link}>
-            Work
-          </Link>
-          <Link to='/about' className={classes.link}>
-            About
-          </Link>
+        <div>
+          <Link to='/'>Work</Link>
+          <Link to='/about'>About</Link>
         </div>
       </Toolbar>
     </AppBar>
