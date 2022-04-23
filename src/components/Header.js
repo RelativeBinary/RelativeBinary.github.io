a5b0db9e7e8983ba0e103f0d5d9979d24b3e482a
RelativeBinary <patrick.bornay@gmail.com>
2022-04-23 15:32:40 +1000

    Apply minor styling to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,13 @@ import Typist from 'react-typist';
 import { ThemeContext, themes } from '../contexts/ThemeContext';
 
 export default function Header() {
-  const [darkMode, setDarkMode] = React.useState(true);
   return (
-    <>
-      <div>
-        <Typist>
-          Hi, I'm
-          <Button className="Button"> Patrick Bornay. </Button>
-        </Typist>
-        <span>
-          <Typist>
-            <Typist.Delay ms={2000} />A web dev & cyber security enthusiast 😄
-          </Typist>
-        </span>
-      </div>
-    </>
+    <div className="Header">
+      <Typist className="HeaderContent">
+        Hi, I'm
+        <Button className="Button"> Patrick Bornay. </Button> <br/>
+        <Typist.Delay ms={2000} />A web dev & cyber security enthusiast 😄
+      </Typist>
+    </div>
   );
 }
