58586833083febb8d828027436d3f3a1c3c6c036
Pat B <patrick.bornay@gmail.com>
2022-04-24 22:34:13 +1000

    minor styling on Projects, ProjectItem and Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,7 @@ export default function Header() {
   return (
     <div className="Header">
       <Typist className="HeaderContent">
-        Hi, I'm
-        <Button className="Button"> Patrick Bornay. </Button> <br/>
+        Hi, I'm Patrick Bornay. <br/>
         <Typist.Delay ms={2000} />A web dev & cyber security enthusiast 😄
       </Typist>
     </div>
