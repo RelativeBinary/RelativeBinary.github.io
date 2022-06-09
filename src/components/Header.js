413274b9a70fc9802ece0969ddaa7a94528c8120
RelativeBinary <patrick.bornay@gmail.com>
2022-06-09 19:00:33 +1000

    move mouse light and navbar to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,16 @@
 import React from 'react';
-import { useState } from 'react';
 import Typist from 'react-typist';
-import Navbar from '../components/Navbar';
 
 export default function Header() {
-  const [MousePosition, setMousePosition] = useState({
-    left: -100,
-    top: -100,
-  });
-
-  const handleMouseMove = (ev) => {
-    setMousePosition({ left: ev.pageX, top: ev.pageY });
-    console.log(MousePosition.left, MousePosition.top)
-  };
 
   return (
-    <div onMouseMove={(ev) => handleMouseMove(ev)} className='Header'>
-      <div
-        className='cursor'
-        style={{
-          position: 'absolute',
-          left: `${MousePosition.left}px`,
-          top: `${MousePosition.top}px`,
-        }}
-      ></div>
+    <div className='Header'>
       <Typist className='HeaderContent'>
         Ahoy There! I'm Patrick. <br />
         <Typist.Delay ms={2000} />A web dev & digital art enthusiast.
       </Typist>
-      <Navbar />
+
+      
       <div id="canvas-container"></div>
       <script src='js/useThreeJS'></script>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
 import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
+  const [MousePosition, setMousePosition] = useState({
+    left: -100,
+    top: -100,
+  });
+
+  const handleMouseMove = (ev) => {
+    setMousePosition({ left: ev.pageX, top: ev.pageY });
+    console.log(MousePosition.left, MousePosition.top)
+  };
+
   return (
-    <div>
+    <div onMouseMove={(ev) => handleMouseMove(ev)} className='Header'>
+      <div
+        className='cursor'
+        style={{
+          position: 'absolute',
+          left: `${MousePosition.left}px`,
+          top: `${MousePosition.top}px`,
+        }}
+      ></div>
       <Header />
+      <Navbar />
       {/* <Footer /> */}
     </div>
   );
