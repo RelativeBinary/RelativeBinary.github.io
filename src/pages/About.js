d6a977886078dea8a7b69ffd0c15f11f07ac3498
Pat B <patrick.bornay@gmail.com>
2022-06-18 21:37:32 +1000

    progress on about page implementation, adding content basic

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Button } from '@mui/material';
 import NavButton from '../components/NavButton';
 import { useState } from 'react';
 import FadeIn from '../components/animation/FadeIn';
+import aboutPrimary from '../images/cross-walk.png'
+import aboutSecondary from '../images/sky.png'
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 
 export default function About() {
    const [MousePosition, setMousePosition] = useState({
@@ -30,6 +32,13 @@ export default function About() {
 
       <div className='about-text'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
+      </div>
+
+      <img className='about-primary-pic' src={aboutPrimary} alt='about primary'/>
+      <img className='about-secondary-pic' src={aboutSecondary} alt='about secondary'/>
+
+      <div className='about-bar scroll-left'>
+          <span>Some text <FiberManualRecordIcon/> Some text <FiberManualRecordIcon/></span>
       </div>
 
       <div className='navbar'>
