a82c55971c3f5e0f8540a5fdd6fb5e61cfae4e1a
Pat B <patrick.bornay@gmail.com>
2022-06-29 08:00:52 +1000

    styling via absolutes, yes I know this is a bad idea, but just for now

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,11 @@
-import React from "react";
-import NavButton from "../components/NavButton";
-import { useState } from "react";
-import FadeIn from "../components/animation/FadeIn";
-import aboutPrimary from "../images/cross-walk.png";
-import aboutSecondary from "../images/sky.png";
-import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
-import Marquee from "react-fast-marquee";
+import React from 'react';
+import NavButton from '../components/NavButton';
+import { useState } from 'react';
+import FadeIn from '../components/animation/FadeIn';
+import aboutPrimary from '../images/cross-walk.png';
+import aboutSecondary from '../images/sky.png';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import Marquee from 'react-fast-marquee';
 
 export default function About() {
   const [MousePosition, setMousePosition] = useState({
@@ -19,20 +19,18 @@ export default function About() {
   };
 
   return (
-    <div onMouseMove={(ev) => handleMouseMove(ev)} className="body">
+    <div onMouseMove={(ev) => handleMouseMove(ev)} className='body'>
       <FadeIn>
         <div
-          className="cursor"
+          className='cursor'
           style={{
-            position: "absolute",
+            position: 'absolute',
             left: `${MousePosition.left}px`,
             top: `${MousePosition.top}px`,
           }}
         ></div>
       </FadeIn>
-      <div className="about-text-pic-wrapper">
-        <div className="about-text-wrapper">
-          <div className="about-text">
+        <div className='about-text'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
           turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
           fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus
@@ -44,50 +42,44 @@ export default function About() {
           lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in
           elementum tellus.
         </div>
-        </div>
-
-        <div className="about-pic-wrapper">
-          <img
-            className="about-primary-pic"
-            src={aboutPrimary}
-            alt="about primary"
-          />
-          <img
-            className="about-secondary-pic"
-            src={aboutSecondary}
-            alt="about secondary"
-          />
-        </div>
-      </div>
-
+      <img
+        className='about-primary-pic'
+        src={aboutPrimary}
+        alt='about primary'
+      />
+      <img
+        className='about-secondary-pic'
+        src={aboutSecondary}
+        alt='about secondary'
+      />
       <Marquee
-        className="about-bar"
+        className='about-bar'
         gradient={false}
         pauseOnHover={true}
         speed={80}
       >
         <FiberManualRecordIcon />
-        <a href="#123" className="about-bar-option">
+        <a href='#123' className='about-bar-option'>
           &nbsp; Linkedin &nbsp;
         </a>
         <FiberManualRecordIcon />
-        <a href="#123" className="about-bar-option">
+        <a href='#123' className='about-bar-option'>
           &nbsp; Github &nbsp;
         </a>
         <FiberManualRecordIcon />
-        <a href="#123" className="about-bar-option">
+        <a href='#123' className='about-bar-option'>
           &nbsp; Resume &nbsp;
         </a>
         <FiberManualRecordIcon />
-        <a href="#123" className="about-bar-option">
+        <a href='#123' className='about-bar-option'>
           &nbsp; Twitter? &nbsp;
         </a>
       </Marquee>
 
-      <div className="navbar">
-        <NavButton name={"Home"} destination={"/"} />
-        <NavButton name={"Works"} destination={"/"} />
-        <NavButton name={"About"} destination={"/"} active={true} />
+      <div className='navbar'>
+        <NavButton name={'Home'} destination={'/'} />
+        <NavButton name={'Works'} destination={'/'} />
+        <NavButton name={'About'} destination={'/'} active={true} />
       </div>
     </div>
   );
