ebfc35fb0fa01a580ccf5decc42a12459175cb21
Pat B <patrick.bornay@gmail.com>
2023-03-05 21:48:06 +1100

    improve about section and README file

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import Typist from "react-typist";
-import NavButton from "../components/NavButton";
-import FadeIn from "../components/animation/FadeIn";
+import React, { useState } from 'react';
+import Typist from 'react-typist';
+import NavButton from '../components/NavButton';
+import FadeIn from '../components/animation/FadeIn';
 import moon from '../images/moon.jpg';
 import Marquee from 'react-fast-marquee';
-import FiberManualRecordIcon from "react-fast-marquee";
+import FiberManualRecordIcon from 'react-fast-marquee';
 
 export default function About() {
   const [MousePosition, setMousePosition] = useState({
@@ -18,33 +18,41 @@ export default function About() {
   };
 
   return (
-    <div onMouseMove={(ev) => handleMouseMove(ev)} className="body">
+    <div onMouseMove={(ev) => handleMouseMove(ev)} className='body'>
       <FadeIn>
         <div
-          className="cursor"
+          className='cursor'
           style={{
-            position: "absolute",
+            position: 'absolute',
             left: `${MousePosition.left}px`,
             top: `${MousePosition.top}px`,
           }}
         ></div>
       </FadeIn>
-      <Typist className="about-text" avgTypingDelay={40}>
-        My primary intention of this site was more to explore the art style of a 
+      <Typist className='about-text' avgTypingDelay={40}>
+        My primary intention for this site was to explore the art style of a
         particular playstation 2 video game, hope you can guess it.
         <Typist.Delay ms={500} />
         <br />
         <br />
-        Feel free to check out my  
-        <a href="https://github.com/RelativeBinary" target="_blank" className="about-link">
+        Feel free to check out my
+        <a
+          href='https://github.com/RelativeBinary'
+          target='_blank'
+          className='about-link'
+        >
           &nbsp; Github &nbsp;
-        </a> 
-        / 
-        <a href="https://www.linkedin.com/in/patrick-bornay-2ab4b013b/" target="_blank" className="about-link">
+        </a>
+        /
+        <a
+          href='https://www.linkedin.com/in/patrick-bornay-2ab4b013b/'
+          target='_blank'
+          className='about-link'
+        >
           &nbsp; Linkedin &nbsp;
-        </a> 
+        </a>
         for all the things,
-        <Typist.Delay ms={1000} /> professionally speaking...
+        <Typist.Delay ms={500} /> professionally speaking...
         <br />
         <br />
         <Typist.Delay ms={1500} />
@@ -52,12 +60,12 @@ export default function About() {
       </Typist>
 
       <FadeIn>
-        <img className='about-image' src={moon} alt='birds'/>
+        <img className='about-image' src={moon} alt='birds' />
       </FadeIn>
 
-      <div className="navbar">
-        <NavButton name={"Home"} destination={"/"} />
-        <NavButton name={"About"} destination={"/"} active={true} />
+      <div className='navbar'>
+        <NavButton name={'Home'} destination={'/'} />
+        <NavButton name={'About'} destination={'/'} active={true} />
       </div>
     </div>
   );
